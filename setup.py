7d1797009f8b4d5502453c10462ea64c4656ec2d
tk0miya <i.tkomiya@gmail.com>
2011-01-08 19:31:55 +0900

    * Add project skelton for seqdiag.

---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os, sys
+
+version = '0.1.0'
+long_description = \
+        open(os.path.join("src","README.txt")).read() + \
+        open(os.path.join("src","TODO.txt")).read()
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Python Software Foundation License",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Documentation",
+    "Topic :: Text Processing :: Markup",
+]
+
+setup(
+     name='seqdiag',
+     version=version,
+     description='seqdiag generate sequence-diagram image file from spec-text file.',
+     long_description=long_description,
+     classifiers=classifiers,
+     keywords=['diagram','generator'],
+     author='Takeshi Komiya',
+     author_email='i.tkomiya at gmail.com',
+     url='https://bitbucket.org/tk0miya/seqdiag',
+     license='PSL',
+     packages=find_packages('src'),
+     package_dir={'': 'src'},
+     package_data = {'': ['buildout.cfg']},
+     include_package_data=True,
+     install_requires=[
+        'setuptools',
+        'PIL',
+        'blockdiag',
+        'funcparserlib',
+         # -*- Extra requirements: -*-
+     ],
+     extras_require=dict(
+         test=[
+             'Nose',
+             'minimock',
+             'pep8',
+         ],
+     ),
+     test_suite='nose.collector',
+     tests_require=['Nose','minimock','pep8'],
+     entry_points="""
+        [console_scripts]
+        seqdiag = seqdiag:main
+     """,
+)
+
