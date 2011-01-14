7d23c9f051039156eb3f443333109c19d3ff2e82
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-01-14 19:24:53 +0900

    * Separate DiagramEdge definition to seqdiag.elements

--- src/seqdiag/seqdiag.py
+++ src/seqdiag/seqdiag.py
@@ -6,79 +6,11 @@ import re
 import sys
 from ConfigParser import SafeConfigParser
 from optparse import OptionParser
-from blockdiag.elements import *
+from elements import *
 import blockdiag.DiagramDraw
 import diagparser
 from blockdiag.utils.XY import XY
 
-# blockdiag patch 1: accept 'return' edge attribute like [return = "foo bar"]
-DiagramEdgeBase = DiagramEdge
-
-
-class DiagramEdge(DiagramEdgeBase):
-    return_label = None
-
-    def __init__(self, node1, node2):
-        DiagramEdgeBase.__init__(self, node1, node2)
-
-        self.height = 1
-        self.y = 0
-        self.async = False
-        self.diagonal = False
-        self.return_label = ''
-
-    def setAttributes(self, attrs):
-        attrs = list(attrs)
-        for attr in list(attrs):
-            value = unquote(attr.value)
-
-            if attr.name == 'return':
-                self.return_label = value
-                attrs.remove(attr)
-            elif attr.name == 'diagonal':
-                self.diagonal = True
-                self.height = 1.5
-                attrs.remove(attr)
-            elif attr.name == 'async':
-                self.dir = 'forward'
-                attrs.remove(attr)
-            elif attr.name == 'dir':
-                dir = value.lower()
-                if dir in ('back', 'both', 'forward'):
-                    self.dir = dir
-                elif dir == '=>':
-                    self.dir = 'both'
-                elif dir in ('->', '->>', '-->', '-->>'):
-                    self.dir = 'forward'
-
-                    if re.search('--', dir):
-                        self.style = 'dashed'
-                    else:
-                        self.style = None
-
-                    if re.search('>>', dir):
-                        self.async = True
-                    else:
-                        self.async = False
-                elif dir in ('<-', '<<-', '<--', '<<--'):
-                    self.dir = 'back'
-
-                    if re.search('--', dir):
-                        self.style = 'dashed'
-                    else:
-                        self.style = None
-
-                    if re.search('<<', dir):
-                        self.async = True
-                    else:
-                        self.async = False
-                else:
-                    msg = "WARNING: unknown edge dir: %s\n" % dir
-                    sys.stderr.write(msg)
-
-                attrs.remove(attr)
-
-        DiagramEdgeBase.setAttributes(self, attrs)
 
 # blocdiag patch 2: add align option for TextFolder
 try:
