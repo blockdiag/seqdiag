ac1ef932fac8d891e95fefba6f52ae68d0d12798
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-07-04 00:16:30 +0900

    * Update version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
      keywords=['diagram','generator'],
      author='Takeshi Komiya',
      author_email='i.tkomiya at gmail.com',
-     url='https://bitbucket.org/tk0miya/seqdiag',
+     url='https://blockdiag.com/',
      license='Apache License 2.0',
      py_modules=['sphinxcontrib_seqdiag', 'seqdiag_sphinxhelper'],
      packages=find_packages('src'),
