f8881261757338f6733bc017d1a9ae12365063a6
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-11-09 19:08:10 +0900

    Remove unsed importing and variables

--- src/seqdiag/DiagramMetrics.py
+++ src/seqdiag/DiagramMetrics.py
@@ -13,6 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import sys
 import elements
 import blockdiag.DiagramMetrics
 from blockdiag.utils import Box, XY
