b5217c919d0ed955232b28afd681eb12d848f326
IGARASHI Masanao <syoux2@gmail.com>
2013-09-20 08:59:21 +0900

    print is a function

--- src/seqdiag/tests/utils.py
+++ src/seqdiag/tests/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import re
 import sys
 from StringIO import StringIO
@@ -26,12 +27,12 @@ def stderr_wrapper(func):
             stderr = sys.stderr
             sys.stderr = StringIO()
 
-            print args, kwargs
+            print(args, kwargs)
             func(*args, **kwargs)
         finally:
             if sys.stderr.getvalue():
-                print "---[ stderr ] ---"
-                print sys.stderr.getvalue()
+                print("---[ stderr ] ---")
+                print(sys.stderr.getvalue())
 
             sys.stderr = stderr
 
@@ -54,13 +55,13 @@ def __validate_node_attributes(filename, **kwargs):
 
     for name, values in kwargs.items():
         if re.match('edge_', name):
-            print "[%s]" % name
+            print("[%s]" % name)
             name = re.sub('edge_', '', name)
             for (id1, id2), value in values.items():
                 found = False
                 for edge in diagram.edges:
                     if edge.node1.id == id1 and edge.node2.id == id2:
-                        print edge
+                        print(edge)
                         eq_(value, getattr(edge, name))
                         found = True
 
@@ -68,8 +69,8 @@ def __validate_node_attributes(filename, **kwargs):
                     raise RuntimeError('edge (%s -> %s) is not found' %
                                        (id1, id2))
         else:
-            print "[node.%s]" % name
+            print("[node.%s]" % name)
             for node in (n for n in diagram.nodes if n.drawable):
-                print node
+                print(node)
                 value = getattr(node, name)
                 eq_(values[node.id], value)
