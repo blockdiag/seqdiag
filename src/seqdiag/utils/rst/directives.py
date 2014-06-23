4e44ca5e034b50ddca6007280ad0c0c793dcc38d
tk0miya <i.tkomiya@gmail.com>
2014-06-23 22:36:36 +0900

    Add options like figure directive to seqdiag directive

--- src/seqdiag/utils/rst/directives.py
+++ src/seqdiag/utils/rst/directives.py
@@ -13,13 +13,11 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import os
-from docutils import nodes
 from docutils.parsers import rst
-from seqdiag import parser
-from seqdiag.builder import ScreenNodeBuilder
-from seqdiag.drawer import DiagramDraw
-from seqdiag.utils.rst.nodes import seqdiag
+import seqdiag.parser
+import seqdiag.builder
+import seqdiag.drawer
+from seqdiag.utils.rst.nodes import seqdiag as seqdiag_node
 from blockdiag.utils.rst import directives
 
 
@@ -33,76 +31,14 @@ directive_options_default = dict(format='PNG',
 directive_options = {}
 
 
-class SeqdiagDirectiveBase(directives.BlockdiagDirectiveBase):
-    """ Directive to insert arbitrary dot markup. """
-    name = "seqdiag"
-    node_class = seqdiag
-
-
 class SeqdiagDirective(directives.BlockdiagDirective):
     name = "seqdiag"
-    node_class = seqdiag
+    node_class = seqdiag_node
+    processor = seqdiag
 
     @property
     def global_options(self):
         return directive_options
-
-    def node2diagram(self, node):
-        try:
-            tree = parser.parse_string(node['code'])
-        except:
-            code = 'seqdiag { %s }' % node['code']
-            tree = parser.parse_string(code)
-            node['code'] = code  # replace if suceeded
-
-        return ScreenNodeBuilder.build(tree)
-
-    def node2image(self, node, diagram):
-        options = node['options']
-        filename = self.image_filename(node)
-        fontmap = self.create_fontmap()
-        _format = self.global_options['format'].lower()
-
-        if _format == 'svg' and self.global_options['inline_svg'] is True:
-            filename = None
-
-        kwargs = dict(self.global_options)
-        del kwargs['format']
-        drawer = DiagramDraw(_format, diagram, filename,
-                             fontmap=fontmap, **kwargs)
-
-        if filename is None or not os.path.isfile(filename):
-            drawer.draw()
-            content = drawer.save(None)
-
-            if _format == 'svg' and self.global_options['inline_svg'] is True:
-                size = drawer.pagesize()
-                if 'maxwidth' in options and options['maxwidth'] < size[0]:
-                    ratio = float(options['maxwidth']) / size[0]
-                    new_size = (options['maxwidth'], int(size[1] * ratio))
-                    content = drawer.save(new_size)
-
-                return nodes.raw('', content, format='html')
-
-        size = drawer.pagesize()
-        if 'maxwidth' in options and options['maxwidth'] < size[0]:
-            ratio = float(options['maxwidth']) / size[0]
-            thumb_size = (options['maxwidth'], size[1] * ratio)
-
-            thumb_filename = self.image_filename(node, prefix='_thumb')
-            if not os.path.isfile(thumb_filename):
-                drawer.filename = thumb_filename
-                drawer.draw()
-                drawer.save(thumb_size)
-
-            image = nodes.image(uri=thumb_filename, target=filename)
-        else:
-            image = nodes.image(uri=filename)
-
-        if node['alt']:
-            image['alt'] = node['alt']
-
-        return image
 
 
 def setup(**kwargs):
